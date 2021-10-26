371543fc6bdc8a714e317608632b7919f87c9645
arty sark <arty.sark@gmail.com>
2021-10-26 16:22:53 +0300

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankAccount = new BankAccount("Arty", 10000);
+            Console.WriteLine(
+                $"A new bank account {bankAccount.Number} was created for {bankAccount.Owner} with ${bankAccount.Balance}");
+
+            bankAccount.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(bankAccount.Balance);
+
+        }
+    }
+}
---
+++ MySuperBank/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySuperBank
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
